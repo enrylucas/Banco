0d2c974c631f3c43d8b184fe7832f75a7f16e103
Enry Goncalves <enry_lucas@hotmail.com>
2018-09-20 19:52:26 -0300

    correcoes gabrier

--- src/main/scala/br/edu/unifei/darksouls3/Arma.scala
+++ src/main/scala/br/edu/unifei/darksouls3/Arma.scala
@@ -3,14 +3,15 @@ package br.edu.unifei.darksouls3
 import scala.slick.driver.PostgresDriver.simple._
 
 
-class Arma(tag: Tag) extends Table[(String,Float,Float,Float,Boolean)](tag,"arma"){
+class Arma(tag: Tag) extends Table[(String,Float,Float,Float,Float,Boolean)](tag,"arma"){
   def nome = column[String]("nome",O.PrimaryKey)
   def peso = column[Float]("peso")
   def durabilidade = column[Float]("durabilidade")
-  def defesaFisica = column[Float]("ataqueFisico")
+  def ataqueFisico = column[Float]("ataqueFisico")
+  def ataqueMagico = column[Float]("ataqueMagico")
   def quebrado = column[Boolean]("quebrado")
 
-  def * = (nome, peso, durabilidade, defesaFisica, quebrado)
+  def * = (nome, peso, durabilidade, ataqueFisico, ataqueMagico, quebrado)
 
 
 }
--- src/main/scala/br/edu/unifei/darksouls3/Armadura.scala
+++ src/main/scala/br/edu/unifei/darksouls3/Armadura.scala
@@ -3,14 +3,15 @@ package br.edu.unifei.darksouls3
 import scala.slick.driver.PostgresDriver.simple._
 
 
-class Armadura(tag: Tag) extends Table[(String,Float,Float,Int,Boolean)](tag,"armadura"){
+class Armadura(tag: Tag) extends Table[(String,Float,Float,Int,Int,Boolean)](tag,"armadura"){
   def nome = column[String]("nome",O.PrimaryKey)
   def peso = column[Float]("peso")
   def durabilidade = column[Float]("durabilidade")
-  def ataqueFisico = column[Int]("ataqueFisico")
+  def defesaFisica = column[Int]("defesaFisica")
+  def defesaMagica = column[Int]("defesaMagica")
   def quebrado = column[Boolean]("quebrado")
 
-  def * = (nome, peso, durabilidade, ataqueFisico, quebrado)
+  def * = (nome, peso, durabilidade, defesaFisica, defesaMagica, quebrado)
 
 
 }
