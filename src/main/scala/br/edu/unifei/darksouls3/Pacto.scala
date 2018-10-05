0ba98f01d3e1303ee68ec5f46d41f8029235607a
gmoreira <castramo@hotmail.com>
2018-10-05 17:42:40 -0300

    AparenciaDao, uma ideia, e mudança no app aplicando

---
+++ src/main/scala/br/edu/unifei/darksouls3/Pacto.scala
@@ -0,0 +1,13 @@
+package br.edu.unifei.darksouls3
+
+
+
+import scala.slick.driver.PostgresDriver.simple._
+
+class Pacto(tag: Tag) extends Table[(String,String)](tag,"pacto"){
+  def nome = column[String]("nome",O.PrimaryKey)
+  def itemPacto = column[String]("itemPacto")
+
+
+  def * = (nome,itemPacto)
+}
---
+++ src/main/scala/br/edu/unifei/darksouls3/PactoHumanoide.scala
@@ -0,0 +1,18 @@
+package br.edu.unifei.darksouls3
+import scala.slick.driver.PostgresDriver.simple._
+
+class PactoHumanoide (tag: Tag) extends Table[(String,String)](tag,"pactoHumanoide"){
+  val pactos = TableQuery[Pacto]
+  val humanoides = TableQuery[Humanoide]
+
+  def pactoNome = column[String]("pactoNome")
+  def humanoideNome = column[String]("humanoideNome")
+
+  def * = (pactoNome,humanoideNome)
+  def pk = primaryKey("primaryKey",(pactoNome,humanoideNome))
+
+  def pacFK = foreignKey("pacto_FK", pactoNome, pactos)(_.nome)
+  def humFK = foreignKey("humanoide_FK", humanoideNome, humanoides)(_.nome)
+
+
+}
