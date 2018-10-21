92340489f449f32c6210ffacabeabaf47e011894
Enry Goncalves <enry_lucas@hotmail.com>
2018-10-21 14:14:51 -0300

    AparenciaDao com cria e deleta

--- src/main/scala/br/edu/unifei/darksouls3/dao/AparenciaDao.scala
+++ src/main/scala/br/edu/unifei/darksouls3/dao/AparenciaDao.scala
@@ -1,7 +1,6 @@
 package br.edu.unifei.darksouls3.dao
 
 import br.edu.unifei.darksouls3.models.Aparencia
-
 import scala.slick.driver.PostgresDriver.simple._
 
 object AparenciaDao {
@@ -21,6 +20,12 @@ object AparenciaDao {
     } yield a
   }
 
+  def criaAparencia(row: (String,String,String,String,String,Float,String))(implicit s: Session) = {
+    aparencias += (0,row._1,row._2,row._3,row._4,row._5,row._6,row._7)
+  }
 
+  def deletaAparencia(id: Int)(implicit s: Session) = {
+    aparencias.filter(_.idVisual === id).delete
+  }
 
 }
